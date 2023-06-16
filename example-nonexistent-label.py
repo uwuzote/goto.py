b1a940334f5dccabf349bc938b85f34ad8676b6c
Xenia <shlykov_misha_2008@mail.ru>
2023-06-16 19:25:24 +0300

    feat: better explanation for example

--- example-nonexistent-label.py
+++ example-nonexistent-label.py
@@ -1,5 +1,17 @@
 from goto import label, goto;
 
+"""
+This example shows how goto tracks
+jumps to non-existent labels.
+This will end with (roughtly):
+```
+Exception ignored in: <function Labeller.__del__ at 0x000000000000>
+Traceback (most recent call last):
+  File "/path/to/goto.py", line 000, in __del__
+goto.UnknownLabelException: Requested jump to non-existent label: 'nowhere'
+```
+"""
+
 @label
 def leads_to_nowhere():
 	goto("nowhere");
