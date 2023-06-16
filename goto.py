082642f667c906dc889b3bca1d31e943332058fb
Xenia <shlykov_misha_2008@mail.ru>
2023-06-16 15:49:37 +0300

    feat: docs and api change

--- goto.py
+++ goto.py
@@ -1,21 +1,52 @@
-class UnknownLabelException(Exception): pass;
+class UnknownLabelException(Exception):
+    """
+    `goto("non-existent_label")` was called.
+    """
+    __slots__ = ();
+
+    pass;
 
 class Goto(BaseException):
+    """
+    Consider using `goto` (notice
+    the lowercase) instead.
+
+    Requested jump to other block.
+    Can only be used in blocks.
+
+    It is actually a exception which
+    need to be raised. 
+    """
     __slots__ = ("label", );
 
     def __init__(self, label: str):
+        assert(isinstance(label, str));
         self.label = label;
 
-goto = Goto;
-del Goto;
-
 class Labeller():
+    """
+    Consider using pre-initialised instance `label`.
+    
+    Singleton. Use as decorator on blocks:
+    ```python
+    @label
+    def loop():
+        print("I am true Chad");
+        goto("loop");
+    ```
+
+    When this object is garbage-collected, it
+    checks if there were jumps to non-existent labels.
+    """
     __slots__ = ("__blocks", "__pending", );
     
     def __init__(self):
         self.__blocks = [];
         self.__pending = None;
 
+    """
+    Use as decorator.
+    """
     def __call__(self, fn):
         label = fn.__name__;
         block = (label, fn);
@@ -26,8 +57,7 @@ class Labeller():
             self.__pending = None;
             self.__execute(block);
 
-        else:
-            pass;
+        return fn;
 
     def __execute(self, block):
         while True:
@@ -72,19 +102,28 @@ class Labeller():
     def __execute_fn(fn):
         try:
             fn();
-        except goto as goto_:
-            return goto_.label;
+        except Goto as goto:
+            return goto.label;
         else:
             return None;
 
+    """
+    Checks if there were jumps to non-existent labels
+    """
     def __del__(self):
         if(self.__pending is not None):
             raise UnknownLabelException(
-                f"Unknown label: {self.__pending !r}"
+                "Requested jump to non-existent "
+                f"label, {self.__pending !r}"
             );
 
 label = Labeller();
-del Labeller;
 
-__all__ = ("label", "goto", "UnknownLabelException", );
-        
+def goto(label: str):
+    """
+    Jump to other block. Can only be used
+    in blocks.
+    """
+    raise Goto(label);
+
+__all__ = ("label", "goto", );
